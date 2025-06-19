6ddc288cf8d64b3b2a2a05119235d0b82d6408d6
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2025-06-19 15:09:54 +0530

    intial update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,51 @@
+import { IBM_Plex_Mono } from "next/font/google";
 import Image from "next/image";
+
+const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className={`flex flex-col items-center justify-center min-h-screen p-4 sm:p-8 pb-20 sm:pb-20 ${ibmPlexMono.className}`}>
+      <div className="mb-8 sm:mb-12 w-full flex justify-center">
+         <div className="w-60 h-60 sm:w-[400px] sm:h-[400px] md:w-[600px] md:h-[600px] relative">
+           <Image src="/image.svg" alt="Construction" fill className="object-contain" />
+         </div>
+     </div>
+      <span className="text-center text-lg sm:text-2xl md:text-4xl mt-4 sm:mt-8 text-blue-500">
+        This site will be under construction for 5 days, Please come back later!      
+      </span>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      
+      <div className="flex flex-col sm:flex-row items-center gap-6 sm:gap-10 mt-8 sm:mt-10 w-full justify-center">
+        <div className="mt-5 sm:mt-0 text-base sm:text-lg font-semibold">Quick links : </div>
+        <div className="relative group flex items-center">
+          <Image src="/google.svg" alt="Gmail" width={40} height={40} className="cursor-pointer w-8 h-8 sm:w-12 sm:h-12" />
+          <div className="absolute left-1/2 -translate-x-1/2 top-full mt-2 sm:mt-5 w-max px-3 py-1 rounded bg-gray-800 text-white text-xs sm:text-md opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none z-10 shadow-lg">
+            vinaykumar.09032002@gmail.com
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="relative group flex items-center">
+          <a href="https://www.linkedin.com/in/vinay-kumar-l/">
+            <Image src="/link.svg" alt="LinkedIn" width={40} height={40} className="cursor-pointer w-8 h-8 sm:w-12 sm:h-12" />
+          </a>
+          <div className="absolute left-1/2 -translate-x-1/2 top-full mt-2 sm:mt-5 w-max px-3 py-1 rounded bg-gray-800 text-white text-xs sm:text-md opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none z-10 shadow-lg">
+            linkedin.com/vinay-kumar-l
+          </div>
+          
+        </div>
+         <div className="relative group flex items-center">
+          <a href="https://www.github.com/v-shadowmaster/">
+            <Image src="/github.svg" alt="Github" width={50} height={50} className="cursor-pointer w-10 h-10 sm:w-[70px] sm:h-[70px]" />
+          </a>
+          <div className="absolute left-1/2 -translate-x-1/2 top-full mt-2 sm:mt-5 w-max px-3 py-1 rounded bg-gray-800 text-white text-xs sm:text-md opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none z-10 shadow-lg">
+            v-shadowmaster
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
